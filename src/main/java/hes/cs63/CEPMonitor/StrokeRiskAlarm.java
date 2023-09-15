7d9383bd9aff4989e25df4d1bdd59fe6f83e61a5
Ishaan Adarsh <ishaanad9@gmail.com>
2023-09-15 13:30:50 +0530

    new

--- src/main/java/hes/cs63/CEPMonitor/StrokeRiskAlarm.java
+++ src/main/java/hes/cs63/CEPMonitor/StrokeRiskAlarm.java
@@ -1,94 +1,25 @@
 package hes.cs63.CEPMonitor;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.restartstrategy.RestartStrategies;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.api.common.time.Time;
-import org.apache.flink.api.common.typeinfo.TypeInformation;
 import org.apache.flink.cep.CEP;
 import org.apache.flink.cep.pattern.Pattern;
-import org.apache.flink.cep.PatternSelectFunction;
 import org.apache.flink.cep.PatternStream;
+import org.apache.flink.cep.PatternSelectFunction;
 import org.apache.flink.cep.pattern.conditions.SimpleCondition;
 import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.connector.kafka.source.KafkaSource;
-import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.connector.kafka.source.reader.deserializer.KafkaRecordDeserializationSchema;
-import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
-import org.apache.flink.streaming.connectors.kafka.config.StartupMode;
-import org.apache.flink.util.Collector;
-import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.StringDeserializer;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
-import java.util.concurrent.TimeUnit;
 
 public class StrokeRiskAlarm {
-
-    private static KafkaSource<HealthData> kafkaSource;
-
-    private static KafkaSource<HealthData> setKafkaSource() {
-        String geCEP;
-        List<String> streamList;
-
-        geCEP = "geCEP";
-
-        streamList = new ArrayList<>();
-        streamList.add(geCEP);
-
-        kafkaSource = KafkaSource.<String>builder()
-                .setTopics(streamList)
-                .setClientIdPrefix("geCEP")
-                .setStartingOffsets(OffsetsInitializer.earliest())
-                .setDeserializer(kafkaRecordDeserializationSchema)
-                .setProperty("partition.discovery.interval.ms",
-                        "300000")
-                .setProperty("commit.offsets.on.checkpoint",
-                        "true")
-                .setProperty("register.consumer.metrics",
-                        "true")
-                .setProperties(getConsumerProps())
-                .build();
-
-        return kafkaSource;
-    }
-
-    public static final KafkaRecordDeserializationSchema kafkaRecordDeserializationSchema = new KafkaRecordDeserializationSchema<HealthData>() {
-        private ObjectMapper mapper = new ObjectMapper();
-
-        @Override
-        public void deserialize(ConsumerRecord<byte[], byte[]> record, Collector<HealthData> out) throws IOException {
-            try {
-                HealthData eventDataMapping = mapper.readValue(record.value(), HealthData.class);
-
-                out.collect(eventDataMapping);
-            } catch (Exception ex) {
-                System.out.println("Exception " + ex.getStackTrace());
-            }
-        }
-
-        @Override
-        public TypeInformation<HealthData> getProducedType() {
-            return TypeInformation.of(HealthData.class);
-        }
-    };
-
-    public static Properties getConsumerProps() {
-
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("group.id", "test-consumer-group");
-        props.put("enable.auto.commit", true);
-        // props.put("security.protocol", "SASL_SSL");
-        return props;
-    }
-
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
@@ -96,43 +27,36 @@ public class StrokeRiskAlarm {
         properties.setProperty("bootstrap.servers", "localhost:9092");
         properties.setProperty("group.id", "test-consumer-group");
 
-        env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3,
-                Time.of(60, TimeUnit.SECONDS)));
+        KafkaSource<String> source = KafkaSource.<String>builder()
+                .setBootstrapServers("localhost:9092")
+                .setGroupId("test-consumer-group")
+                .setTopics(Arrays.asList("geCEP"))
+                .setDeserializer(KafkaRecordDeserializationSchema.valueOnly(StringDeserializer.class))
+                //.setStartingOffsets(OffsetsInitializer.earliest())
+                .setValueOnlyDeserializer(new SimpleStringSchema())
+                .build();
+
+        DataStreamSource<String> patientData = env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
 
         // Use KafkaSource
-        DataStream<HealthData> patientData = env
-                .fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "Kafka Source")
-                .setParallelism(2)
-                .uid("test");
-        patientData.print();
-
-        // DataStream<String> patientData = env.fromSource(
-        // KafkaSource.<String>builder()
-        // .setBootstrapServers("localhost:9092")
-        // .setGroupId("test-consumer-group")
-        // .setTopics(Arrays.asList("geCEP"))
-        // .setDeserializer(KafkaRecordDeserializationSchema.valueOnly(StringDeserializer.class))
-        // .setStartingOffsets(OffsetsInitializer.earliest())
-        // .build(),
-        // "Kafka Source");
-
-        Pattern<String, ?> highRiskPattern = Pattern.<String>begin("first").where(new SimpleCondition<String>() {
-            @Override
-            public boolean filter(String value) {
-                return getValue(value) > 3;
-            }
-        });
+        Pattern<String, ?> highRiskPattern = Pattern.<String>begin("first")
+                .where(new SimpleCondition<String>() {
+                    @Override
+                    public boolean filter(String value) {
+                        return getValue(value) > 3;
+                    }
+                });
 
         PatternStream<String> patternStream = CEP.pattern(
                 patientData,
-                highRiskPattern);
+                highRiskPattern
+        );
 
         DataStream<String> strokeRiskAlerts = patternStream.select(new PatternSelectFunction<String, String>() {
             @Override
             public String select(Map<String, List<String>> pattern) throws Exception {
                 String userId = pattern.get("first").get(0).split(",")[0].trim();
-                int risk = getTotalRisk(pattern.get("first").get(0), pattern.get("middle").get(0),
-                        pattern.get("last").get(0));
+                int risk = getTotalRisk(pattern.get("first").get(0), pattern.get("middle").get(0), pattern.get("last").get(0));
                 return "Stroke Risk Alert: Patient ID - " + userId + ", Risk Level - " + risk;
             }
         });
@@ -140,7 +64,6 @@ public class StrokeRiskAlarm {
         strokeRiskAlerts.print();
 
         env.execute("Stroke Risk Alert Job");
-
     }
 
     private static int getValue(String value) {
@@ -176,18 +99,15 @@ public class StrokeRiskAlarm {
             risk += measurementValue >= 39.1 ? 1 : 0;
             return risk;
         }
-
         // Default risk calculation
         return 0;
     }
 
     private static int getTotalRisk(String firstValue, String middleValue, String lastValue) {
-        // Calculate the total risk based on the values of the first, middle, and last
-        // measurements
+        // Calculate the total risk based on the values of the first, middle, and last measurements
         int firstRisk = getValue(firstValue);
         int middleRisk = getValue(middleValue);
         int lastRisk = getValue(lastValue);
-
         return firstRisk + middleRisk + lastRisk;
     }
 }
